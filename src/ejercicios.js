ee431e1550a93a1391f04f1607a99a5b36abd744
Celeste Cancio <celestecancio@gmail.com>
2022-01-24 10:42:26 -0300

    Resuelve ejercicios

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,19 +1,50 @@
-export function multiplicarPor10(array) {}
+export function multiplicarPor10(array) {
+    return array.map (numero => numero * 10);
+}
 
-export function soloPares(array) {}
+export function soloPares(array) {
+    return array.filter (numero => numero % 2 == 0);
+}
 
-export function moverALaDerecha(array) {}
+export function moverALaDerecha(array) {
+    return [...(array.slice(array.length-1)),...(array.slice(0,array.length-1))];
+}
 
-export function soloUnaPalabra(array) {}
+export function soloUnaPalabra(array) {
+    return array.filter(string => !string.includes(" "));
+}
 
-export function soloArraysPositivos(array) {}
+export function soloArraysPositivos(array) {
+    return array.filter(subArray => subArray.every(number => number>0));
+}
 
-export function soloVocales(array) {}
+export function soloVocales(array) {
+    const vowels = "aeiouAEIOU";
+    return array.filter(letra => vowels.includes(letra));
+}
 
 export function todasVocalesIguales(array) {}
 
-export function duplicarMatriz(array) {}
+export function duplicarMatriz(array) {
+    return array.map(subArray => subArray.map(numero => numero*2));
+}
 
-export function mayoresDeEdadValidados(array) {}
+
+export function mayoresDeEdadValidados(array) {
+    //Se puede hacer todo en la linea del return con ternario?
+        
+    function devolverObjetoValidado (objeto){
+        if (objeto.edad>18){
+            return {...objeto, mayor:true};
+        } 
+        else{
+            return {...objeto, mayor:false};
+        }        
+    }
+    return array.map(devolverObjetoValidado);
+
+    //Esta opcion devuelve solo [true,true,false,true]
+    // return arrayValidado = array.map(objeto => objeto.edad>=18 ? objeto.mayor=true : objeto.mayor=false);
+}
 
 export function transformarObjetoEnArray(objeto) {}
